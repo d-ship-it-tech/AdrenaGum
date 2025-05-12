08ba8ce93b878e5de6f7d71dd6677a22b6a9220f
Yangmin1128 <yali2028@tisbrain.net>
2025-05-12 18:22:23 +0900

    Create product.js

---
+++ product.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.buy').forEach(button => {
+  button.addEventListener('click', function() {
+    window.location.href = 'https://codepen.io/Divisha-Agrawal/full/qEEPBvX';
+  });
+});
